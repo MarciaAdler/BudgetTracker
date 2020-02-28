1fad91a982f1d82cfa4ba0390af3f97ed4cbd45d
Marcia <marciaadler718@gmail.com>
2020-02-28 10:08:21 -0500

    update to mongoDB connection for heroku

--- server.js
+++ server.js
@@ -15,14 +15,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
-
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
-
-mongoose.connect(MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true
+  //useFindAndModify: false
+});
 
 // routes
 app.use(require("./routes/api.js"));
