9784a09356d6a8193fbc7cd854bb4ca8cd151e05
Marcia <marciaadler718@gmail.com>
2020-07-10 16:21:53 -0400

    updated DB config var for update to database connection on heroku) :

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true
+mongoose.connect(process.env.DB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
   // useFindAndModify: false
 });
 
